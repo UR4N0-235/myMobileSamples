84e65bf25f4c5a756e33fa4fbbb6b04f29b18a8c
Matheus Fernandes <matheus.fernandes92.235@protonmail.com>
2023-01-01 23:37:15 -0300

    Initial commit

---
+++ BirthdayMensage/app/src/main/java/com/ur4n0/birthdaymensage/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.ur4n0.birthdaymensage
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.ur4n0.birthdaymensage.ui.theme.BirthdayMensageTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BirthdayMensageTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    BirthdayGreetingWithText("Happy BirthDay for you ! ! !", "- from android apps")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BirthdayGreetingWithText(message: String, from: String) {
+    Column{
+        Text(
+            text = message,
+            fontSize = 36.sp
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun BirthdayCardPreview() {
+    BirthdayMensageTheme {
+        BirthdayGreetingWithText("Happy BirthDay for you ! ! !", "- from android apps")
+    }
+}
